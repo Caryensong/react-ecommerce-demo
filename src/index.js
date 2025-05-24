5d5d2b8e0136b1153057f182793b814b38a64ce0
Caryensong <caryensong@googlemail.com>
2025-05-24 16:02:57 +0200

    import bootstrap navbar

--- src/index.js
+++ src/index.js
@@ -1,6 +1,7 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
 import Navbar from "./components/navbar";
+import "bootstrap/dist/css/bootstrap.min.css";
 
 const element = <h1>Hallo Welt</h1>;
 
