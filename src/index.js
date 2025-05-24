be83a4e90b76de176a1d1c00867fbf35048eac49
Caryensong <caryensong@googlemail.com>
2025-05-24 15:49:15 +0200

    Set up basic React app with Navbar component

--- src/index.js
+++ src/index.js
@@ -1,7 +1,14 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
+import Navbar from "./components/navbar";
 
 const element = <h1>Hallo Welt</h1>;
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
-root.render(element);
+root.render(
+  <div>
+    <Navbar />
+    {element}
+  </div>
+);
+
