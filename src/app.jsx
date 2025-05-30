0f7c312435a2ac79f8a8c1635817d245ef8d368d
Caryensong <caryensong@googlemail.com>
2025-05-30 20:12:25 +0200

    add shopping-cart component

--- src/app.jsx
+++ src/app.jsx
@@ -1,6 +1,7 @@
 import React, { Component } from 'react';
 import Navbar from "./components/navbar";
 import Product from "./components/product";
+import ShoppingCart from './components/shopping-cart';
 
 class App extends Component {
     state = {  }
@@ -8,13 +9,15 @@ render() {
     return (
         <React.Fragment>
             <Navbar />
+            <div className="main-container">
             <div className='product_container'>
                 <Product image="babyschuhe.jpg" title="Baby Schuhe"  description="Füge das Produkt in den Warenkorb hinzu"/>
                 <Product image="chucks.jpg" title="Chucks" description="Füge das Produkt in den Warenkorb hinzu"/>
                 <Product image="nike.jpg" title="Nike run" description="Füge die Nike's in den Warenkorb hinzu"/>
                 <Product image="anzugsschuhe.jpg" title="Leder Schuhe" description="Füge das Produkt in den Warenkorb hinzu"/>
             </div>
-            
+            <ShoppingCart />
+            </div>
         </React.Fragment>
     );
 }
