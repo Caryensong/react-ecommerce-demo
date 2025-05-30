aa2c8b7384497f5f2415d83b044019b652ce7586
Caryensong <caryensong@googlemail.com>
2025-05-30 22:24:03 +0200

    render shopping-cart

--- src/components/navbar.jsx
+++ src/components/navbar.jsx
@@ -3,13 +3,13 @@ import React, { Component } from 'react';
 class Navbar extends Component {
     state = {  } 
     render() { 
-        return <nav class="navbar navbar-expand-lg bg-body-tertiary">
+        return <nav className="navbar navbar-expand-lg bg-body-tertiary">
   <div className="container-fluid">
     <a className="navbar-brand" href="#">Navbar</a>
     <button className="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNavAltMarkup" aria-controls="navbarNavAltMarkup" aria-expanded="false" aria-label="Toggle navigation">
       <span className="navbar-toggler-icon"></span>
     </button>
-    <div clasNames="collapse navbar-collapse" id="navbarNavAltMarkup">
+    <div classNames="collapse navbar-collapse" id="navbarNavAltMarkup">
       <div className="navbar-nav">
         <a className="nav-link active" aria-current="page" href="/">Home</a>
         <a className="nav-link" href="#">Features</a>
--- src/components/shopping-cart.jsx
+++ src/components/shopping-cart.jsx
@@ -1,11 +1,35 @@
 import React, { Component }from "react";
 
 class ShoppingCart extends Component {
-    state = {  } 
+    state = {  
+        items:[
+            {
+                amount: 1,
+                name:'Nike run',
+                price: 120,
+        },
+            {
+                amount: 2,
+                name:'Chucks',
+                price: 80,
+            },
+            {
+                amount: 1,
+                name:'Leder Schuhe',
+                price: 150,
+            },
+            {
+                amount: 1,
+                name:'Baby Schuhe',
+                price: 50,
+            }
+    ]
+    } 
     render() { 
         return (
             <div className="shopping-cart">
                 <h2>Warenkorb</h2>
+               {this.state.items.map( item => <div key={item.name}>{item.amount}x {item.name} {item.price} €</div>)}
             </div>
         );
     }
